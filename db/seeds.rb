9c9e46631840af50f436a2b5525ae575197b8f55
Kenny Wong <kken.w@hotmail.com>
2022-06-01 13:21:56 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+random = ['8','9']
+location = []
+
 5.times do 
   new_user = User.create(
     name: Faker::Name.name
     email: Faker::Internet.email
-    phone_number: 
+    contact_number: "+65" + " " + random.sample + Faker::Number.Number(digits: 7).to_s
+    location: 
+    description: "From" + Faker::Educator.secondary_school
+    role: "100"
   )
 end
 
@@ -23,7 +29,14 @@ create_table "users", force: :cascade do |t|
   t.datetime "remember_created_at"
   t.datetime "created_at", precision: 6, null: false
   t.datetime "updated_at", precision: 6, null: false
+  t.string "username"
+  t.string "location"
+  t.text "description"
+  t.string "role"
+  t.string "contact_number"
   t.index ["email"], name: "index_users_on_email", unique: true
   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 end
+
+
 
