b79f0bac31f35581af83627e323ed1c1422b7dc9
sekkun foo <sekkun.luke@gmail.com>
2022-06-01 17:47:24 +0800

    added subjects function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_065155) do
+ActiveRecord::Schema.define(version: 2022_06_01_094018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,11 +68,9 @@ ActiveRecord::Schema.define(version: 2022_06_01_065155) do
     t.text "description"
     t.float "hourly_rate"
     t.bigint "user_id", null: false
-    t.bigint "booking_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "grade", default: [], array: true
-    t.index ["booking_id"], name: "index_subjects_on_booking_id"
     t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
@@ -96,6 +94,5 @@ ActiveRecord::Schema.define(version: 2022_06_01_065155) do
   add_foreign_key "availabilities", "users"
   add_foreign_key "reviews", "users"
   add_foreign_key "schedules", "users"
-  add_foreign_key "subjects", "bookings"
   add_foreign_key "subjects", "users"
 end
