a187ab265801ebd77806448775b039e8eef380ed
Kenny Wong <kken.w@hotmail.com>
2022-06-01 15:27:00 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,86 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# This is to seed Student
 random = ['8','9']
-location = []
+# Will add more location if need be
+location = ["Jurong", "Tampines", "Yishun", "Labrador", "Raffles Place"]
+# Either have number of no number
+contact_num = ["+65 #{random.sample}#{Faker::Number.number(digits: 7)}", ""]
+# Description
+subject = ["English", "Maths", "Science", "History", "Geography", "Social Studies", "Arts", "A-Math", "Mother Tongue"]
+# grade
+grade = ["Secondary-1", "Secondary-2", "Secondary-3", "Secondary-4/5"]
+# Content
+contents = ["They/Them pretty good!", "We blazed", "Good tutor, will come back again!", "Shit tutor, don't go for this person"]
+# Just playing
+num = 143596825983245
 
-5.times do 
-  new_user = User.create(
-    name: Faker::Name.name
-    email: Faker::Internet.email
-    contact_number: "+65" + " " + random.sample + Faker::Number.Number(digits: 7).to_s
-    location: 
-    description: "From" + Faker::Educator.secondary_school
-    role: "100"
+puts "Seeding your shit"
+puts "Go get a drink, its gonna take years"
+100.times do
+  new_user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    contact_number: "#{contact_num.sample}",
+    location: "#{location.sample}",
+    description: "From #{Faker::Educator.secondary_school} seeking tuition for #{subject.sample}",
+    role: "Student",
+    password: "password"
   )
+  new_user.save!
+  puts "1 down, #{num -= 1} more to go!"
 end
 
+50.times do
+  locate = location.sample
+  subs = subject.sample
+  new_user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    contact_number: "#{contact_num.sample}",
+    location: "#{locate}",
+    description: "From #{Faker::Educator.university} teaching #{subject.sample}",
+    role: "Student",
+    password: "password"
+  )
+  new_user.save!
 
-create_table "users", force: :cascade do |t|
-  t.string "email", default: "", null: false
-  t.string "encrypted_password", default: "", null: false
-  t.string "reset_password_token"
-  t.datetime "reset_password_sent_at"
-  t.datetime "remember_created_at"
-  t.datetime "created_at", precision: 6, null: false
-  t.datetime "updated_at", precision: 6, null: false
-  t.string "username"
-  t.string "location"
-  t.text "description"
-  t.string "role"
-  t.string "contact_number"
-  t.index ["email"], name: "index_users_on_email", unique: true
-  t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  # Seed for subjects
+  count = 0
+  num = (101..151).to_a.sample
+  new_subject = Subject.new(
+    name: "#{subs}",
+    grade: "#{grade.sample}",
+    user_id: "#{num}".to_i,
+    description: "Much #{subs} very wow",
+    hourly_rate: Faker::Number.number(digits: 10).round,
+    # booking_id: "#{count += 1}".to_i
+  )
+  new_subject.save!
+
+  new_booking = Booking.new(
+    subject_id: "#{count += 1}".to_i
+  )
+  puts "1 down, #{num -= 1} more to go!"
 end
+
+# Seed for review
+10.times do
+  content: "#{contents.sample}"
+  rating: (1..5).to_a
+end
+
+puts "Finished, now get back to work!!"
+puts "jk, love you <3"
+
+# create_table "bookings", force: :cascade do |t|
+#   t.datetime "start_datetime"
+#   t.datetime "end_datetime"
+#   t.boolean "confirmed", default: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
 
 
 
