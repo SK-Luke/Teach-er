5508d5351375eb42c5ea5b6c730e235cea3d4448
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-02 22:43:30 +0800

    resolved conflict

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -14,7 +14,8 @@ class SchedulesController < ApplicationController
   def index
     # This section is for the calendar view
     start_date = params.fetch(:start_date, Date.today).to_date
-    @availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    #@availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @availability_slot = Availability.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
 
     # This section is for adding a new availability
     @new_availability = Availability.new
