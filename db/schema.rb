389ddb3093156dd5f50e43e59066d310f3f6cffc
Victor Duverne <victor.duverne@gmail.com>
2022-06-02 15:51:10 +0800

    changed schema good

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_074627) do
+ActiveRecord::Schema.define(version: 2022_06_02_053257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 2022_06_02_074627) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
+    t.boolean "confirmed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "subject_id", null: false
-    t.string "status", default: "pending"
     t.index ["subject_id"], name: "index_bookings_on_subject_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
