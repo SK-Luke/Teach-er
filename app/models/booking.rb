54c97bd6a7d1670d7023f3f151a4cbf361441a3a
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-03 10:21:43 +0800

    edited booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :student, class_name: "User", foreign_key: "user_id"
 
   STATUS = %w[pending confirmed cancelled completed]
+  GRADES = %w[Secondary-1 Secondary-2 Secondary-3 Secondary-4/5]
   validates :start_time, :end_time, presence: true
   validates :status, inclusion: { in: STATUS }
+  validates :grade, inclusion: { in: GRADES }
 end
