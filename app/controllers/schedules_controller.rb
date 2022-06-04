535b13201a7daf0e05fbe14f4389ff74346beea6
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-05 01:23:46 +0800

    added where clause for teachers month view

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -20,7 +20,7 @@ class SchedulesController < ApplicationController
     user_id = current_user.id
     @availability_slot = Availability.where(start_time: start_date.beginning_of_week..start_date.end_of_week).where(user_id: user_id)
 
-    @month_availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @month_availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week).where(user_id: user_id)
 
     # This section is for adding a new availability
     @new_availability = Availability.new
