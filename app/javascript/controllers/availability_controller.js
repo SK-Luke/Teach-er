d0bb770e6fcdd2c8cb781165e1842826a6d521c1
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-05 18:25:06 +0800

    mass delete function at schedules index page

--- app/javascript/controllers/availability_controller.js
+++ app/javascript/controllers/availability_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
+import { csrfToken } from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["addbtn"]
+  static targets = ["addbtn", "slot", "delBtn", "cfmBtn"]
 
   connect() {
     console.log("hello from availability_controller!")
@@ -16,7 +17,62 @@ export default class extends Controller {
 
   modalClose(event) {
     event.preventDefault();
-    var modal = document.getElementById("addAvailModal");
+    const modal = document.getElementById("addAvailModal");
     modal.style.display = "none";
+  }
+
+  deleteMode(event) {
+    event.preventDefault();
+    const cfmBtn = this.cfmBtnTarget;
+    cfmBtn.style.display = "block";
+    const delBtn = this.delBtnTarget
+    delBtn.innerText = "Delete Mode"
+    this.slotTargets.forEach(slot => {
+      slot.addEventListener("click", function(){
+        if (slot.className === "available") {
+          // if (slot.style.color === "white") {
+          //   console.log(slot)
+          //   slot.style.color = "#53b480";
+          // } else {
+          //   console.log(slot)
+          //   slot.style.color = "white"
+          // }
+          slot.className = "selected"
+        } else {
+          slot.className = "available"
+        }
+       });
+    })
+  }
+
+  deleteSelected(event) {
+    event.preventDefault;
+    let selected = []
+    this.slotTargets.forEach(slot => {
+      console.log(slot.className)
+      if (slot.className === "selected") {
+        console.log(slot)
+        selected.push(slot)
+      }
+    })
+
+    console.log(selected)
+
+    selected.forEach(sel => {
+      console.log(sel.id)
+      fetch(`/availabilities/${sel.id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRF-Token': csrfToken()
+        }
+      })
+      .then(event.target.parentElement.remove())
+      })
+    const cfmBtn = this.cfmBtnTarget;
+    cfmBtn.style.display = "none";
+    const delBtn = this.delBtnTarget
+    delBtn.innerText = "-"
   }
 }
